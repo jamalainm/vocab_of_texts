9a37d23b89c1abbfeac0d0d4976513a933cedd7c
jamalainm <robot@inman.xyz>
2020-07-13 19:26:03 -0500

    New file that polls DB to recover student vocab and patterns learned.

---
+++ get_knowledge.py
@@ -0,0 +1,49 @@
+import mysql.connector
+from unidecode import unidecode
+import json
+
+def get_knowledge():
+    """ poll the db and return a tuple of lists: patterns, vocab """
+
+    with open('info.json','r') as f:
+        data = json.load(f)
+
+    mydb = mysql.connector.connect(
+            host=data['host'],
+            port=data['port'],
+    #        database="curriculum",
+            user=data["user"],
+            password=data["password"],
+            auth_plugin='mysql_native_password'
+            )
+# Get ITTB codes for word forms known
+    grammar = mydb.cursor()
+
+    grammar.execute("SELECT ITTB_Code FROM curriculum.cll_grammar_topics WHERE unit = 1")
+# Get the codes out of tuple and into list
+    patterns = []
+
+    for g in grammar:
+        if len(g) > 0:
+            patterns.append(g[0])
+
+# Get the phrases learned so far
+    vocab = mydb.cursor()
+
+    vocab.execute("SELECT phrase FROM Verba.nineteen")
+# Get the vocab out of tuple and into list
+    phrases = [v[0] for v in vocab]
+# Get rid of slashes, hyphenates; unidecode, and lowercase
+    words = []
+
+    for p in phrases:
+        new_phrase = p.split(' ')
+        for np in new_phrase:
+            np = np.lower()
+            np = unidecode(np)
+            if np[0] != '-' and np not in words:
+                words.append(np)
+
+    return (patterns, words)
+
+
